3f3261f759159de7025366621ff993e5075dba0b
Vinicius Jara <viniciusfjara@gmail.com>
2022-11-06 23:29:01 -0400

    commit inicial, react-router-dom e tailwind css instalados

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-red-500'>Aplicativo de Tickets - Teste de Commit Inicial</h1>
+
+    </div>
+  )
+}
+
+export default App
