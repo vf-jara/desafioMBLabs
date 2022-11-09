23bb8a71925829f8caf0be175768c2b29b5f3583
Vinicius Jara <viniciusfjara@gmail.com>
2022-11-09 16:39:09 -0400

    logo adicionada e ajuste na header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../../assets/images/logo.png'
 
 /*
 *******************************
@@ -13,14 +14,16 @@ const handleSubmit = e => {
 
 function Header() {
     return (
-        <div className='w-full flex flex-1 border-b p-3 bg-[rgba(255,255,255,0.6)] shadow backdrop-blur-sm'>
-            <div className='px-5 py-4 bg-white flex justify-center w-28 md:w-40 border'>
+        <div id='mainHeader' className='w-full flex flex-1 border-b p-3 bg-[rgba(255,255,255,0.6)] shadow backdrop-blur-sm'>
+            <div className=' bg-white flex justify-center w-28 md:w-40 border'>
                 <Link to={"/"}>
-                    <div>LOGO</div>
+                    <div>
+                        <img src={Logo}></img>
+                    </div>
                 </Link>
             </div>
             <div className='flex flex-1'>
-                <form onSubmit={handleSubmit} className='flex w-full'>
+                <form onSubmit={handleSubmit} className='flex w-full lg:justify-center lg:pr-40'>
                     <input className='flex w-full md:w-1/2 px-2 border-2 mx-5 rounded-lg focus:outline-sky-600 outline-offset-0' type="search" placeholder="Buscar eventos" />
                 </form>
             </div>
